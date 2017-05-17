c9150845b3746e1688f4083109a91001e8606a7d
HZFC <502614432@qq.com>
2017-05-17 13:50:24 +0800

    修改返回值类型为int

--- src/main/java/com/gp/controller/RegistGroupController.java
+++ src/main/java/com/gp/controller/RegistGroupController.java
@@ -49,14 +49,16 @@ public class RegistGroupController {
 	}
 	
 		@RequestMapping("/addGroup")
-		public String addGroup(int matchId,int userId,String groupName){
+		public int addGroup(int matchId,int userId,String groupName){
 			GameMatch gm = gameMatchService.findById(matchId);
 			if(gm.getTeamNum() == gm.getGroupList().size()){
 				
-				return "创建队伍失败，比赛组数已到上限";
+				return 2;//2创建队伍失败，比赛组数已到上限
+
 			}else if(ifRegisted(matchId,userId)){
 				
-				return "已经在比赛队伍中";
+				return 3;//3已经在比赛队伍中
+
 					
 			}else{	
 				User u = userService.findById(userId);
@@ -67,27 +69,31 @@ public class RegistGroupController {
 				g.setUserList(userList);
 				gm.addGroup(g);
 				gameMatchService.save(gm);
-				return "创建队伍成功";	
+				return 1;	//1创建队伍成功
+
 			}
 				
 		}
 		
 		@RequestMapping("/addGroupMember")
-		public String addGroupMember(int matchId,int groupId,int userId){
+		public int addGroupMember(int matchId,int groupId,int userId){
 				if(ifRegisted(matchId,userId)){
 				
-					return "已经在比赛队伍中";
+					return 3;//3已经在比赛队伍中
+
 					
 				}else{
 					GameMatch gm = gameMatchService.findById(matchId);
 					RegistGroup g = registGroupService.findById(groupId);
 					User u = userService.findById(userId);
 					if(gm.getPersonNum()==g.getUserList().size()){
-						return "加入队伍失败，队伍人数已到上限";
+						return 2;//2加入队伍失败，队伍人数已到上限
+
 					}else{
 						g.addUser(u);
 						registGroupService.save(g);
-						return "加入队伍成功";
+						return 1;//1加入队伍成功
+
 					}
 					
 				}
