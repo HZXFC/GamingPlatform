1791b921fd8eda890582f39e6d6537509b50ac35
HZFC <502614432@qq.com>
2017-05-17 23:40:48 +0800

    关注比赛controller

---
+++ src/main/java/com/gp/controller/FollowMatchController.java
@@ -0,0 +1,59 @@
+package com.gp.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gp.bean.FollowMatch;
+import com.gp.bean.GameMatch;
+import com.gp.service.FollowMatchService;
+import com.gp.service.GameMatchService;
+
+@RestController
+@RequestMapping("/followMatch")
+public class FollowMatchController {
+	
+	@Resource
+	private FollowMatchService followMatchService;
+	
+	@Resource
+	private GameMatchService gameMatchService;
+	
+	 @RequestMapping("/save")
+	 public int save(int userId,int matchId){
+		 FollowMatch followMatch = new FollowMatch();
+		 followMatch.setUserId(userId);
+		 followMatch.setMatchId(matchId);
+		 followMatchService.save(followMatch);
+		 return 1; 
+	 }
+	
+	 @RequestMapping("/followMatchList")
+	 public List<GameMatch> followMatchList(int userId){
+		 List<FollowMatch> followMatchList = followMatchService.findByUserId(userId);
+		 List<GameMatch> gameMatchList =new ArrayList<>();
+		for(FollowMatch floolwMatch:followMatchList)	{
+			GameMatch gameMatch = new GameMatch();
+			gameMatch=gameMatchService.findById(floolwMatch.getMatchId());
+			gameMatchList.add(gameMatch);
+			
+		}
+		 return gameMatchList;
+	 }
+	 
+	 @RequestMapping("/ifFollowedByMe")
+	 public int ifFollowedByMe(int userId,int matchId){
+		 List<FollowMatch> followMatchList = followMatchService.findByUserId(userId);
+		 for(FollowMatch floolwMatch:followMatchList)	{
+			 if(floolwMatch.getMatchId()==matchId){
+				 return 1;
+			 }
+		 }
+		 return 0;
+	 }
+	 
+}
