f50849cbdac639cc8b5527e20172f4017181e21f
ClearFive <2217350445@qq.com>
2017-06-06 21:53:21 +0800

    错误处理

--- src/main/java/com/gp/controller/GameMatchController.java
+++ src/main/java/com/gp/controller/GameMatchController.java
@@ -1,5 +1,6 @@
 package com.gp.controller;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -31,9 +32,24 @@ public class GameMatchController {
 
 	
 	@RequestMapping("/save")
-	public String save(){
+	public int save(int award,String area,String deadline,String gameName,String matchIMG,String matchName,int ruleID,int personNum,int teamNum,String startTime) {
+//		GameMatch gameMatch = new GameMatch();
+//		@SuppressWarnings("deprecation")
+//		Date d = new Date(deadline);
+//		Date sd = new Date(startTime);
+//		gameMatch.setArea(area);
+//		gameMatch.setAward(award);
+//		gameMatch.setDeadline(d);
+//		gameMatch.setGameName(gameName);
+//		gameMatch.setMatchIMG(matchIMG);
+//		gameMatch.setMatchName(matchName);
+//		gameMatch.setPersonNum(personNum);
+//		gameMatch.setRuleID(ruleID);
+//		gameMatch.setStartTime(sd);
+//		gameMatch.setTeamNum(teamNum);
+//		gameMatchService.save(gameMatch);
 		
-		return "save Ok";
+		return 1;
 	}
 	
 	@RequestMapping("/findByGameName")
